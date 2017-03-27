2cd8e9f290a866d34bfde967786f47c1d6a1021a
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2017-03-27 14:01:10 +0100

    Adds RuboCop and fixes initial offences

--- lib/gs-ruby.rb
+++ lib/gs-ruby.rb
@@ -1,5 +1,8 @@
-require "gs-ruby/version"
+# frozen_string_literal: true
+require 'gs-ruby/version'
 
+# GS Ruby is a simple wrapper for the Ghostscript command - it's assumed
+# that you have the +gs+ command already installed.
 module GS
   # Your code goes here...
 end
