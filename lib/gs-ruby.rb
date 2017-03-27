e2bc31b67406baad71150e75e77139989cba2d26
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2017-03-27 14:05:41 +0100

    Adds a constants module for options names

--- lib/gs-ruby.rb
+++ lib/gs-ruby.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+require 'gs-ruby/options'
 require 'gs-ruby/version'
 
 # GS Ruby is a simple wrapper for the Ghostscript command - it's assumed
 # that you have the +gs+ command already installed.
 module GS
-  # Your code goes here...
+  include Options
 end
---
+++ lib/gs-ruby/options.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+module GS
+  module Options
+    PDFA                      = 'PDFA'
+    BATCH                     = 'BATCH'
+    NO_PAUSE                  = 'NOPAUSE'
+    DEVICE                    = 'DEVICE'
+    PROCESS_COLOR_MODEL       = 'ProcessColorModel'
+    PDFA_COMPATIBILITY_POLICY = 'PDFACompatibilityPolicy'
+    OUTPUT_FILE               = 'OutputFile'
+  end
+end
